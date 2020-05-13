90ec0c6c713a579a401e90ed083213b106e24d62
Tom-Roger Mittag <mittaaku@pm.me>
2020-05-13 11:13:28 +0200

    Initial Commit

---
+++ LogosUtils/LogosUtils.swift
@@ -0,0 +1,11 @@
+//
+//  LogosUtils.swift
+//  LogosUtils
+//
+//  Created by Tom-Roger Mittag on 5/13/20.
+//  Copyright © 2020 TheCrossReference. All rights reserved.
+//
+
+class LogosUtils {
+
+}
