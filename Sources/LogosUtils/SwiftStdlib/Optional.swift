db24a09ba6aba6bbebbd5a5d8c7c8e67667e26e2
Tom-Roger Mittag <mittaaku@pm.me>
2020-05-16 09:52:28 +0200

    Stuff

--- Sources/LogosUtils/SwiftStdlib/Optional.swift
+++ Sources/LogosUtils/SwiftStdlib/Optional.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-extension Optional where Wrapped: Collection {
+public extension Optional where Wrapped: Collection {
     var isNilOrEmpty: Bool {
         return self?.isEmpty ?? true
     }
 }
 
-extension Optional where Wrapped == String {
+public extension Optional where Wrapped == String {
     var isNilOrBlank: Bool {
         return self?.isBlank ?? true
     }
