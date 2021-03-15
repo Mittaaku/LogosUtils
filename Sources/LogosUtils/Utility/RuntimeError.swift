13f18fec02e69cb9ae0e8fcd297a1afede7222f2
Tom-Roger Mittag <mittaaku@pm.me>
2021-03-15 17:09:10 +0100

    Wow. Much cakes.

--- Sources/LogosUtils/Utility/RuntimeError.swift
+++ Sources/LogosUtils/Utility/RuntimeError.swift
@@ -11,7 +11,7 @@ import Foundation
 public struct RuntimeError: Error {
     let message: String
 
-    init(_ message: String) {
+    public init(_ message: String) {
         self.message = message
     }
 
