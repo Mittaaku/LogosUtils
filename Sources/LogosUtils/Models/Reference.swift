fa248e4e220684251991f8a45b451aa5f290d57d
Tom-Roger Mittag <mittag@pm.me>
2022-11-18 14:03:55 +0100

    I have written of great things

--- Sources/LogosUtils/Models/Reference.swift
+++ Sources/LogosUtils/Models/Reference.swift
@@ -6,28 +6,20 @@
 import Foundation
 
 public struct Reference: Codable, Hashable, Identifiable, CustomStringConvertible {
-	public var book: Int
-	public var chapter: Int
-	public var verse: Int
-	public var word: Int
-	public var morpheme: Int
+	public var id: Int
 	
 	// MARK: General initilizers
 	
 	public init(book: Int, chapter: Int, verse: Int, word: Int = 0, morpheme: Int = 0) {
-		self.book = book
-		self.chapter = chapter
-		self.verse = verse
-		self.word = word
-		self.morpheme = morpheme
+		self.id = (book * Reference.bookRadix)
+		+ (chapter * Reference.chapterRadix)
+		+ (verse * Reference.verseRadix)
+		+ (word * Reference.wordRadix)
+		+ (morpheme * Reference.morphemeRadix)
 	}
 	
 	public init(id: Int) {
-		book = id / Self.bookRadix
-		chapter = (id % Self.bookRadix) / Self.chapterRadix
-		verse = (id % Self.chapterRadix) / Self.verseRadix
-		word = (id % Self.verseRadix) / Self.wordRadix
-		morpheme = id % Self.wordRadix
+		self.id = id
 	}
 	
 	// MARK: Codable
@@ -50,77 +42,66 @@ public struct Reference: Codable, Hashable, Identifiable, CustomStringConvertibl
 	
 	// MARK: Identifiable and other IDs
 	
-	public var id: Int {
-		return (book * Reference.bookRadix)
-		+ (chapter * Reference.chapterRadix)
-		+ (verse * Reference.verseRadix)
-		+ (word * Reference.wordRadix)
-		+ morpheme
-	}
-	
 	public var bookId: Int {
-		return (book * Reference.bookRadix)
-		+ (chapter * Reference.chapterRadix)
+		return id & Reference.bookIdBits
 	}
 	
 	public var chapterId: Int {
-		return (book * Reference.bookRadix)
-		+ (chapter * Reference.chapterRadix)
+		return id & Reference.chapterIdBits
 	}
 	
 	public var verseId: Int {
-		return (book * Reference.bookRadix)
-		+ (chapter * Reference.chapterRadix)
-		+ (verse * Reference.verseRadix)
+		return id & Reference.wordIdBits
 	}
 	
 	public var wordId: Int {
-		return (book * Reference.bookRadix)
-		+ (chapter * Reference.chapterRadix)
-		+ (verse * Reference.verseRadix)
-		+ (word * Reference.wordRadix)
+		return id & Reference.wordIdBits
+	}
+	
+	// MARK: Numbers
+	
+	public var bookNumber: Int {
+		return id / Reference.bookRadix
+	}
+	
+	public var chapterNumber: Int {
+		return (id & Reference.chapterBits) / Reference.chapterRadix
+	}
+	
+	public var verseNumber: Int {
+		return (id & Reference.verseBits) / Reference.verseRadix
+	}
+	
+	public var wordNumber: Int {
+		return (id & Reference.wordBits) / Reference.wordRadix
+	}
+	
+	public var morphemeNumber: Int {
+		return id & Reference.morphemeIdBits
 	}
 	
 	// MARK: Conversion
 	
 	public var bookReference: Reference {
-		return Reference(id: bookId)
+		return Reference(id: id & Reference.bookIdBits)
 	}
 	
 	public var chapterReference: Reference {
-		return Reference(id: chapterId)
+		return Reference(id: id & Reference.chapterIdBits)
 	}
 	
 	public var verseReference: Reference {
-		return Reference(id: verseId)
+		return Reference(id: id & Reference.wordIdBits)
 	}
 	
 	public var wordReference: Reference {
-		return Reference(id: wordId)
+		return Reference(id: id & Reference.wordIdBits)
 	}
 	
 	// MARK: CustomStringConvertable
 	
 	public var description: String {
 		return String(id)
-	}
-	
-	// MARK: Equating methods
-	
-	public func sharesBook(with reference: Reference) -> Bool {
-		return bookId == reference.bookId
-	}
-	
-	public func sharesChapter(with reference: Reference) -> Bool {
-		return chapterId == reference.chapterId
-	}
-	
-	public func sharesVerse(with reference: Reference) -> Bool {
-		return verseId == reference.verseId
-	}
-	
-	public func sharesWord(with reference: Reference) -> Bool {
-		return wordId == reference.wordId
 	}
 	
 	public func offset(from reference: Reference) -> Offset {
@@ -144,12 +125,25 @@ public struct Reference: Codable, Hashable, Identifiable, CustomStringConvertibl
 }
 
 public extension Reference {
-	static let invalid      	= Reference(id: 0)
-	static let bookRadix    	= 1000000000000
-	static let chapterRadix 	= 0001000000000
-	static let verseRadix   	= 0000001000000
-	static let wordRadix    	= 0000000000100
-	static let morphemeRadix    = 0000000000001
+	static let invalid	= Reference(id: 0)
+	
+	static let bookRadix				= 0x0100000000
+	static let chapterRadix				= 0x0001000000
+	static let verseRadix				= 0x0000010000
+	static let wordRadix				= 0x0000000100
+	static let morphemeRadix			= 0x0000000001
+	
+	static let bookBits					= 0xFF00000000
+	static let chapterBits				= 0x00FF000000
+	static let verseBits				= 0x0000FF0000
+	static let wordBits					= 0x000000FF00
+	static let morphemeBits				= 0x00000000FF
+	
+	static let bookIdBits				= 0xFF00000000
+	static let chapterIdBits			= 0xFFFF000000
+	static let verseIdBits				= 0xFFFFFF0000
+	static let wordIdBits				= 0xFFFFFFFF00
+	static let morphemeIdBits			= 0xFFFFFFFFFF
 	
 	enum Offset {
 		case differentBook
--- Tests/LogosUtilsTests/OtherTests.swift
+++ Tests/LogosUtilsTests/OtherTests.swift
@@ -14,11 +14,20 @@ final class ReferenceTests: XCTestCase {
 	let reference2 = Reference(book: 3, chapter: 1, verse: 4)
 	let reference3 = Reference(book: 3, chapter: 2, verse: 1)
 	let reference4 = Reference(book: 1, chapter: 2, verse: 1)
+	let reference5 = Reference(book: 1, chapter: 2, verse: 3)
 	
 	func testReferenceOffset() {
 		XCTAssertEqual(reference1.offset(from: reference2), .differentVerse)
 		XCTAssertEqual(reference2.offset(from: reference3), .differentChapter)
 		XCTAssertEqual(reference3.offset(from: reference4), .differentBook)
 		XCTAssertEqual(reference4.offset(from: reference4), .identical)
+		
+		XCTAssertEqual(reference4.bookNumber, 1)
+		XCTAssertEqual(reference4.chapterNumber, 2)
+		XCTAssertEqual(reference4.verseNumber, 1)
+		
+		XCTAssertEqual(reference4.bookId == reference5.bookId, true)
+		XCTAssertEqual(reference4.chapterId == reference5.chapterId, true)
+		XCTAssertEqual(reference4.verseId == reference5.verseId, false)
 	}
 }
