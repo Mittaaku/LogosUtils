068cacaae3706208163b5f4ab0b2812fe631e647
Tom-Roger Mittag <mittag@pm.me>
2022-11-28 08:44:22 +0100

    More work

---
+++ Sources/LogosUtils/Protocols/CodedResource.swift
@@ -0,0 +1,35 @@
+//
+//  LogosUtils
+//  Copyright Tom-Roger Mittag 2022.
+//
+
+import Foundation
+
+protocol CodedResource: Decodable {
+}
+
+extension CodedResource {
+	static func decodeArray(fromJson json: String, inBundle bundle: Bundle? = nil) -> Array<Self> {
+		return decode(fromJson: json)
+	}
+	
+	static func decodeElement(fromJson json: String, inBundle bundle: Bundle? = nil) -> Self {
+		return decode(fromJson: json)
+	}
+	
+	private static func decode<T: Decodable>(fromJson json: String, inBundle bundle: Bundle? = nil) -> T {
+		do {
+			let bundle = bundle ?? Bundle.main
+			guard let url = Bundle.main.url(forResource: json, withExtension: "json") else {
+				fatalError("Json resource '\(json)' not found")
+			}
+			let data = try Data(contentsOf: url)
+			let decoder = JSONDecoder()
+			let result = try decoder.decode(T.self, from: data)
+			return result
+			
+		} catch let error {
+			fatalError(error.localizedDescription)
+		}
+	}
+}
