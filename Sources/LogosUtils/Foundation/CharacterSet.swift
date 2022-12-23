f067f0f3471b237dd3944990e19c33a283f37366
Tom-Roger Mittag <mittag@pm.me>
2022-12-23 11:36:46 +0100

    Work

--- Sources/LogosUtils/Foundation/CharacterSet.swift
+++ Sources/LogosUtils/Foundation/CharacterSet.swift
@@ -17,17 +17,4 @@ public extension CharacterSet {
 
 // MARK: Methods
 public extension CharacterSet {
-	
-	func containsPrecomposedUnicodeScalar(of scalar: UnicodeScalar) -> Bool {
-		return containsUnicodeScalars(of: String(scalar), precomposed: true)
-	}
-	
-	func containsUnicodeScalar(of character: Character, precomposed: Bool = true) -> Bool {
-		return containsUnicodeScalars(of: String(character), precomposed: precomposed)
-	}
-	
-	func containsUnicodeScalars(of string: String, precomposed: Bool = true) -> Bool {
-		let stringToEvaluate = precomposed ? string.precomposedStringWithCanonicalMapping : string
-		return stringToEvaluate.unicodeScalars.allSatisfy(contains(_:))
-	}
 }
